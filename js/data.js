59faf89929bdc6a06c84a38f60fae18ad5934387
Kardast <dottorsf@gmail.com>
2022-05-19 18:21:37 +0200

    Exercise done

--- js/data.js
+++ js/data.js
@@ -116,16 +116,51 @@ const myArr = [
 
 // selecting the main container
 const myContainer = document.querySelector(".my_container");
-const selectBtn = document.querySelector("button");
-console.log(selectBtn);
 const selectCateg = document.getElementById("category");
 console.log(selectCateg);
 
-selectBtn.addEventListener("click",
+for (let i = 0; i < myArr.length; i++) {
+	
+	// creating the cards
+	let myCard = document.createElement("div");
+	myCard.classList.add("my_card");
+	myContainer.append(myCard);
+	// console.log(myCard);
+
+	// creating the container for the content
+	let myContent = document.createElement("div");
+	myContent.classList.add("my_content");
+	myCard.append(myContent);
+
+	// creating the card's content (i tag and h3 tag)
+		// i tag
+	let myIcon = document.createElement("i");
+	myIcon.classList.add("fas", "fa-" + myArr[i].name);
+	myContent.append(myIcon);
+	if (myArr[i].color === "orange") {
+		myIcon.classList.add("orange");
+	} else if (myArr[i].color === "green") {
+		myIcon.classList.add("green");
+	} else if (myArr[i].color === "blue") {
+		myIcon.classList.add("blue");
+	};
+	// console.log(myIcon);
+
+		// h3 tag
+	let myH3 = document.createElement("h3");
+	myContent.append(myH3);
+	let myH3Text = myArr[i].name;
+	myH3.append(myH3Text);
+	// console.log(myH3Text);
+};
+
+selectCateg.addEventListener("change",
 	function() {
 		
 		const chooseCateg = parseInt(selectCateg.value);
         console.log(chooseCateg);
+
+		myContainer.innerHTML = "";
 
 		switch (chooseCateg) {
 			case 0:
@@ -247,7 +282,7 @@ selectBtn.addEventListener("click",
 				})
 				console.log(arrVegetable);
                 break;
-				
+
             case 3:
 				const arrUser = myArr.filter((element) => {
 					if (element.type === "user") {
@@ -291,10 +326,3 @@ selectBtn.addEventListener("click",
         }
 	}
 )
-
-
-
-
-// in base al value assegnato alla categoria vai a creare un ciclo filter diverso
-// dall'array che scaturisce dal ciclo filter fai partire un ciclo la cui lunghezza varierà in base al contenuto del nuovo array
-// vai a printare il contenuto del nuovo array
