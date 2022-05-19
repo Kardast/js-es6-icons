d3b8ec8426f9d0562166ca667b10efd0ca601007
Kardast <dottorsf@gmail.com>
2022-05-19 17:49:44 +0200

    Selecting category

--- js/data.js
+++ js/data.js
@@ -116,43 +116,6 @@ const myArr = [
 
 // selecting the main container
 const myContainer = document.querySelector(".my_container");
-
-for (let i = 0; i < myArr.length; i++) {
-	
-	// creating the cards
-	let myCard = document.createElement("div");
-	myCard.classList.add("my_card");
-	myContainer.append(myCard);
-	// console.log(myCard);
-
-	// creating the container for the content
-	let myContent = document.createElement("div");
-	myContent.classList.add("my_content");
-	myCard.append(myContent);
-
-	// creating the card's content (i tag and h3 tag)
-		// i tag
-	let myIcon = document.createElement("i");
-	myIcon.classList.add("fas", "fa-" + myArr[i].name);
-	myContent.append(myIcon);
-	if (myArr[i].color === "orange") {
-		myIcon.classList.add("orange");
-	} else if (myArr[i].color === "green") {
-		myIcon.classList.add("green");
-	} else if (myArr[i].color === "blue") {
-		myIcon.classList.add("blue");
-	};
-	// console.log(myIcon);
-
-		// h3 tag
-	let myH3 = document.createElement("h3");
-	myContent.append(myH3);
-	let myH3Text = myArr[i].name;
-	myH3.append(myH3Text);
-	// console.log(myH3Text);
-
-}
-
 const selectBtn = document.querySelector("button");
 console.log(selectBtn);
 const selectCateg = document.getElementById("category");
@@ -165,73 +128,172 @@ selectBtn.addEventListener("click",
         console.log(chooseCateg);
 
 		switch (chooseCateg) {
-            case 0:
-				console.log("ciao");
-                break;
+			case 0:
+				for (let i = 0; i < myArr.length; i++) {
+	
+					// creating the cards
+					let myCard = document.createElement("div");
+					myCard.classList.add("my_card");
+					myContainer.append(myCard);
+					// console.log(myCard);
+				
+					// creating the container for the content
+					let myContent = document.createElement("div");
+					myContent.classList.add("my_content");
+					myCard.append(myContent);
+				
+					// creating the card's content (i tag and h3 tag)
+						// i tag
+					let myIcon = document.createElement("i");
+					myIcon.classList.add("fas", "fa-" + myArr[i].name);
+					myContent.append(myIcon);
+					if (myArr[i].color === "orange") {
+						myIcon.classList.add("orange");
+					} else if (myArr[i].color === "green") {
+						myIcon.classList.add("green");
+					} else if (myArr[i].color === "blue") {
+						myIcon.classList.add("blue");
+					};
+					// console.log(myIcon);
+				
+						// h3 tag
+					let myH3 = document.createElement("h3");
+					myContent.append(myH3);
+					let myH3Text = myArr[i].name;
+					myH3.append(myH3Text);
+					// console.log(myH3Text);
+				
+				}
+				break;
+
             case 1:
-
+				const arrAnimals = myArr.filter((element) => {
+					if (element.type === "animal") {
+						// creating the cards
+						let myCard = document.createElement("div");
+						myCard.classList.add("my_card");
+						myContainer.append(myCard);
+						// console.log(myCard);
+				
+						// creating the container for the content
+						let myContent = document.createElement("div");
+						myContent.classList.add("my_content");
+						myCard.append(myContent);
+				
+						// creating the card's content (i tag and h3 tag)
+							// i tag
+						let myIcon = document.createElement("i");
+						myIcon.classList.add("fas", "fa-" + element.name);
+						myContent.append(myIcon);
+				
+						if (element.color === "orange") {
+							myIcon.classList.add("orange");
+						} else if (element.color === "green") {
+							myIcon.classList.add("green");
+						} else if (element.color === "blue") {
+							myIcon.classList.add("blue");
+						};
+				
+						// console.log(myIcon);
+							// h3 tag
+						let myH3 = document.createElement("h3");
+						myContent.append(myH3);
+						let myH3Text = element.name;
+						myH3.append(myH3Text);
+						// console.log(myH3Text);
+					}
+					return false;
+				})
+				console.log(arrAnimals);
                 break;
-            case 2:
 
+            case 2:
+				const arrVegetable = myArr.filter((element) => {
+					if (element.type === "vegetable") {
+						// creating the cards
+						let myCard = document.createElement("div");
+						myCard.classList.add("my_card");
+						myContainer.append(myCard);
+						// console.log(myCard);
+				
+						// creating the container for the content
+						let myContent = document.createElement("div");
+						myContent.classList.add("my_content");
+						myCard.append(myContent);
+				
+						// creating the card's content (i tag and h3 tag)
+							// i tag
+						let myIcon = document.createElement("i");
+						myIcon.classList.add("fas", "fa-" + element.name);
+						myContent.append(myIcon);
+				
+						if (element.color === "orange") {
+							myIcon.classList.add("orange");
+						} else if (element.color === "green") {
+							myIcon.classList.add("green");
+						} else if (element.color === "blue") {
+							myIcon.classList.add("blue");
+						};
+				
+						// console.log(myIcon);
+							// h3 tag
+						let myH3 = document.createElement("h3");
+						myContent.append(myH3);
+						let myH3Text = element.name;
+						myH3.append(myH3Text);
+						// console.log(myH3Text);
+					}
+					return false;
+				})
+				console.log(arrVegetable);
+                break;
+				
+            case 3:
+				const arrUser = myArr.filter((element) => {
+					if (element.type === "user") {
+						// creating the cards
+						let myCard = document.createElement("div");
+						myCard.classList.add("my_card");
+						myContainer.append(myCard);
+						// console.log(myCard);
+				
+						// creating the container for the content
+						let myContent = document.createElement("div");
+						myContent.classList.add("my_content");
+						myCard.append(myContent);
+				
+						// creating the card's content (i tag and h3 tag)
+							// i tag
+						let myIcon = document.createElement("i");
+						myIcon.classList.add("fas", "fa-" + element.name);
+						myContent.append(myIcon);
+				
+						if (element.color === "orange") {
+							myIcon.classList.add("orange");
+						} else if (element.color === "green") {
+							myIcon.classList.add("green");
+						} else if (element.color === "blue") {
+							myIcon.classList.add("blue");
+						};
+				
+						// console.log(myIcon);
+							// h3 tag
+						let myH3 = document.createElement("h3");
+						myContent.append(myH3);
+						let myH3Text = element.name;
+						myH3.append(myH3Text);
+						// console.log(myH3Text);
+					}
+					return false;
+				})
+				console.log(arrUser);
                 break;
         }
 	}
 )
 
-const arrAnimals = myArr.filter((first) => {
-    if (first.type === "animal") {
-		// creating the cards
-		let myCard = document.createElement("div");
-		myCard.classList.add("my_card");
-		myContainer.append(myCard);
-		// console.log(myCard);
 
-		// creating the container for the content
-		let myContent = document.createElement("div");
-		myContent.classList.add("my_content");
-		myCard.append(myContent);
 
-		// creating the card's content (i tag and h3 tag)
-			// i tag
-		let myIcon = document.createElement("i");
-		myIcon.classList.add("fas", "fa-" + myArr[0].name);
-		myContent.append(myIcon);
-
-		if (myArr[0].color === "orange") {
-			myIcon.classList.add("orange");
-		} else if (myArr[0].color === "green") {
-			myIcon.classList.add("green");
-		} else if (myArr[0].color === "blue") {
-			myIcon.classList.add("blue");
-		};
-
-		// console.log(myIcon);
-			// h3 tag
-		let myH3 = document.createElement("h3");
-		myContent.append(myH3);
-		let myH3Text = myArr[0].name;
-		myH3.append(myH3Text);
-		// console.log(myH3Text);
-    }
-    return false;
-})
-console.log(arrAnimals);
-
-const arrVegetable = myArr.filter((first) => {
-    if (first.type === "vegetable") {
-        return true;
-    }
-    return false;
-})
-console.log(arrVegetable);
-
-const arrUser = myArr.filter((first) => {
-    if (first.type === "user") {
-        return true;
-    }
-    return false;
-})
-console.log(arrUser);
 
 // in base al value assegnato alla categoria vai a creare un ciclo filter diverso
 // dall'array che scaturisce dal ciclo filter fai partire un ciclo la cui lunghezza varierà in base al contenuto del nuovo array
