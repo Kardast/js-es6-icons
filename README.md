e5175c6766204c943aeb357d1e7ceb5ee71d3a9b
Kardast <dottorsf@gmail.com>
2022-05-19 17:36:42 +0200

    Trying filter cycle

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni ic
 
 Milestone 2
 Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+<!-- OK -->
 
 Milestone 3
 Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
--- js/data.js
+++ js/data.js
@@ -113,11 +113,9 @@ const myArr = [
 	}
 ];
 
-// console.log(myArr);
 
 // selecting the main container
 const myContainer = document.querySelector(".my_container");
-// console.log(myContainer);
 
 for (let i = 0; i < myArr.length; i++) {
 	
@@ -137,7 +135,6 @@ for (let i = 0; i < myArr.length; i++) {
 	let myIcon = document.createElement("i");
 	myIcon.classList.add("fas", "fa-" + myArr[i].name);
 	myContent.append(myIcon);
-
 	if (myArr[i].color === "orange") {
 		myIcon.classList.add("orange");
 	} else if (myArr[i].color === "green") {
@@ -145,8 +142,8 @@ for (let i = 0; i < myArr.length; i++) {
 	} else if (myArr[i].color === "blue") {
 		myIcon.classList.add("blue");
 	};
-
 	// console.log(myIcon);
+
 		// h3 tag
 	let myH3 = document.createElement("h3");
 	myContent.append(myH3);
@@ -156,4 +153,86 @@ for (let i = 0; i < myArr.length; i++) {
 
 }
 
+const selectBtn = document.querySelector("button");
+console.log(selectBtn);
+const selectCateg = document.getElementById("category");
+console.log(selectCateg);
 
+selectBtn.addEventListener("click",
+	function() {
+		
+		const chooseCateg = parseInt(selectCateg.value);
+        console.log(chooseCateg);
+
+		switch (chooseCateg) {
+            case 0:
+				console.log("ciao");
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+        }
+	}
+)
+
+const arrAnimals = myArr.filter((first) => {
+    if (first.type === "animal") {
+		// creating the cards
+		let myCard = document.createElement("div");
+		myCard.classList.add("my_card");
+		myContainer.append(myCard);
+		// console.log(myCard);
+
+		// creating the container for the content
+		let myContent = document.createElement("div");
+		myContent.classList.add("my_content");
+		myCard.append(myContent);
+
+		// creating the card's content (i tag and h3 tag)
+			// i tag
+		let myIcon = document.createElement("i");
+		myIcon.classList.add("fas", "fa-" + myArr[0].name);
+		myContent.append(myIcon);
+
+		if (myArr[0].color === "orange") {
+			myIcon.classList.add("orange");
+		} else if (myArr[0].color === "green") {
+			myIcon.classList.add("green");
+		} else if (myArr[0].color === "blue") {
+			myIcon.classList.add("blue");
+		};
+
+		// console.log(myIcon);
+			// h3 tag
+		let myH3 = document.createElement("h3");
+		myContent.append(myH3);
+		let myH3Text = myArr[0].name;
+		myH3.append(myH3Text);
+		// console.log(myH3Text);
+    }
+    return false;
+})
+console.log(arrAnimals);
+
+const arrVegetable = myArr.filter((first) => {
+    if (first.type === "vegetable") {
+        return true;
+    }
+    return false;
+})
+console.log(arrVegetable);
+
+const arrUser = myArr.filter((first) => {
+    if (first.type === "user") {
+        return true;
+    }
+    return false;
+})
+console.log(arrUser);
+
+// in base al value assegnato alla categoria vai a creare un ciclo filter diverso
+// dall'array che scaturisce dal ciclo filter fai partire un ciclo la cui lunghezza varierà in base al contenuto del nuovo array
+// vai a printare il contenuto del nuovo array
